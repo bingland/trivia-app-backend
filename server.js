1c664ee4c14b64c5bf054ed18cd9402bc29f4148
Brayden England <bray.patriot@gmail.com>
2021-06-12 22:51:12 -0600

    Made routes, get info from db

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ mongoose.connect(
   }
 )
 
+// schemas
+const User = require('./models/User')
+const Score = require('./models/Score')
+
 // middleware
 app.use(express.urlencoded({extended: false}))
 app.use(cors()) // can add origin / credentials in object for params
@@ -35,6 +39,39 @@ app.use(session({
 app.use(flash())
 app.use(passport.initialize())
 app.use(passport.session())
+
+// routes
+app.get('/scores', (req, res) => {
+  console.log('get /scores')
+  // get scores from DB. public.
+  // req.body get scores from categories / difficulties
+
+  console.log(req.body.difficulty)
+
+  Score.find({categoryID: req.body.category})
+  .populate({path: 'scores', populate: { path: 'user', model: 'User' }})
+  .exec((err, data) => {
+    console.log(data)
+    res.json(data)
+  })
+})
+
+app.post('/addscore', (req, res) => {
+  console.log('post /score')
+})
+
+app.post('/register', (req, res) => {
+  console.log('post /register')
+})
+
+app.post('/login', (req, res) => {
+  console.log('post /login')
+})
+
+// TODO: do trivia api calls from the server w/ session
+app.post('/answer', (req, res) => {
+  console.log('post /answer')
+})
 
 // listen
 app.listen(port, () => console.log(`✨ Server listening on port ${port}`))
